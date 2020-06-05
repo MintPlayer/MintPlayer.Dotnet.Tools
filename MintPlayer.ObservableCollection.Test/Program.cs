8c451bbe48c993e87ab00154505898edbb483258
Pieterjan De Clippel <pieterjandeclippel@hotmail.com>
2020-06-05 10:20:28 +0200

    Transfer package to .NET Standard

--- MintPlayer.ObservableCollection.Test/Program.cs
+++ MintPlayer.ObservableCollection.Test/Program.cs
@@ -15,35 +15,46 @@ namespace MintPlayer.ObservableCollection.Test
 
         private static void Demo2()
         {
-            var collection = new ObservableCollection<Person>();
-            collection.CollectionChanged += (sender, e) =>
-            {
-                Console.WriteLine($"Collection changed:");
-                if (!e.NewItems.IsNullOrEmpty())
-                {
-                    var newItemsArray = new Person[e.NewItems.Count];
-                    e.NewItems.CopyTo(newItemsArray, 0);
-                    Console.WriteLine($"- items added: {string.Join(", ", newItemsArray.Select(p => p.FullName))}");
-                }
-                if (!e.OldItems.IsNullOrEmpty())
-                {
-                    var oldItemsArray = new Person[e.OldItems.Count];
-                    e.OldItems.CopyTo(oldItemsArray, 0);
-                    Console.WriteLine($"- items removed: {string.Join(", ", oldItemsArray.Select(p => p.FullName))}");
-                }
-            };
-            collection.ItemPropertyChanged += (sender, e) =>
-            {
-                Console.WriteLine($"Item property changed: {e.PropertyName}");
-            };
 
-            collection.AddRange(new[] {
-                new Person { FirstName = "John", LastName = "Doe" },
-                new Person { FirstName = "Jimmy", LastName = "Fallon" },
-                new Person { FirstName = "Michael", LastName = "Douglas" }
-            });
 
-            collection[1].LastName = "Knibble";
+    var collection = new ObservableCollection<Person>();
+    collection.CollectionChanged += (sender, e) =>
+    {
+        Console.WriteLine($"Collection changed:");
+        if (!e.NewItems.IsNullOrEmpty())
+        {
+            var newItemsArray = new Person[e.NewItems.Count];
+            e.NewItems.CopyTo(newItemsArray, 0);
+            Console.WriteLine($"- items added: {string.Join(", ", newItemsArray.Select(p => p.FullName))}");
+        }
+        if (!e.OldItems.IsNullOrEmpty())
+        {
+            var oldItemsArray = new Person[e.OldItems.Count];
+            e.OldItems.CopyTo(oldItemsArray, 0);
+            Console.WriteLine($"- items removed: {string.Join(", ", oldItemsArray.Select(p => p.FullName))}");
+        }
+    };
+    collection.ItemPropertyChanged += (sender, e) =>
+    {
+        Console.WriteLine($"Item property changed: {e.PropertyName}");
+    };
+
+    var person1 = new Person { FirstName = "John", LastName = "Doe" };
+    var person2 = new Person { FirstName = "Jimmy", LastName = "Fallon" };
+    var person3 = new Person { FirstName = "Michael", LastName = "Douglas" };
+
+    // Add 3 items at once
+    collection.AddRange(new[] {
+        person1,
+        person2,
+        person3
+    });
+
+    // Change an item property
+    collection[1].LastName = "Knibble";
+
+    // Remove 2 items at once
+    collection.RemoveRange(new[] { person1, person3 });
         }
 
         //private static void Demo1()
--- MintPlayer.ObservableCollection/ObservableCollection.cs
+++ MintPlayer.ObservableCollection/ObservableCollection.cs
@@ -8,28 +8,43 @@ namespace MintPlayer.ObservableCollection
 {
     public class ObservableCollection<T> : System.Collections.ObjectModel.ObservableCollection<T>
     {
-        private bool isAddingRange = false;
+        private bool isAddingOrRemovingRange = false;
 
         public void AddRange(IEnumerable<T> items)
         {
             try
             {
-                isAddingRange = true;
+                isAddingOrRemovingRange = true;
                 foreach (var item in items)
                     Add(item);
             }
             finally
             {
-                isAddingRange = false;
+                isAddingOrRemovingRange = false;
                 OnCollectionChanged(new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Add, items.ToList()));
+            }
+        }
+
+        public void RemoveRange(IEnumerable<T> items)
+        {
+            try
+            {
+                isAddingOrRemovingRange = true;
+                foreach (var item in items)
+                    Remove(item);
+            }
+            finally
+            {
+                isAddingOrRemovingRange = false;
+                OnCollectionChanged(new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Remove, items.ToList()));
             }
         }
 
         protected override void OnCollectionChanged(NotifyCollectionChangedEventArgs e)
         {
-            if (!isAddingRange)
+            if (!isAddingOrRemovingRange)
             {
-                // Only invoke the PropertyChanged with all items, not individually
+                // Only invoke the CollectionChanged with all items, not individually
                 base.OnCollectionChanged(e);
 
                 // Also only attach the PropertyChanged event handler when we're not into
@@ -39,6 +54,7 @@ namespace MintPlayer.ObservableCollection
                 itf.CopyTo(itf_arr, 0);
                 if (itf_arr.Contains(typeof(System.ComponentModel.INotifyPropertyChanged)))
                 {
+                    // First detach all event handlers
                     if (e.OldItems != null)
                     {
                         foreach (var item in e.OldItems)
@@ -46,6 +62,7 @@ namespace MintPlayer.ObservableCollection
                             ((System.ComponentModel.INotifyPropertyChanged)item).PropertyChanged -= ObservableCollection_Item_PropertyChanged;
                         }
                     }
+                    // Then attach all event handlers
                     if (e.NewItems != null)
                     {
                         foreach (var item in e.NewItems)
@@ -62,11 +79,13 @@ namespace MintPlayer.ObservableCollection
             OnItemPropertyChanged((T)sender, e.PropertyName);
         }
 
+        #region ItemPropertyChanged
         public event ItemPropertyChangedEventHandler<T> ItemPropertyChanged;
         protected void OnItemPropertyChanged(T item, string propertyName/*, object oldValue, object newValue*/)
         {
             if (ItemPropertyChanged != null)
                 ItemPropertyChanged(this, new Events.EventArgs.ItemPropertyChangedEventArgs<T>(item, propertyName/*, oldValue, newValue*/));
         }
+        #endregion
     }
 }
