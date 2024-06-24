047b5b292244e07565cf1f9162be2f0ec8d873d3
Pieterjan De Clippel <pieterjandeclippel@msn.com>
2024-06-24 10:25:17 +0200

    Improve hasher

--- FolderHasher/MintPlayer.FolderHasher.Abstractions/IFolderHasher.cs
+++ FolderHasher/MintPlayer.FolderHasher.Abstractions/IFolderHasher.cs
@@ -1,6 +1,10 @@
-﻿namespace MintPlayer.FolderHasher.Abstractions;
+﻿using System.Security.Cryptography;
+
+namespace MintPlayer.FolderHasher.Abstractions;
 
 public interface IFolderHasher
 {
     Task<string> GetFolderHashAsync(string folder);
+    Task<string> GetFolderHashAsync(string folder, IEnumerable<string> ignoreFolders);
+    Task<string> GetFolderHashAsync(string folder, IEnumerable<string> ignoreFolders, HashAlgorithm algorithm);
 }
