06f7916164f7ba743a7578fe7ee4ff30ba2747ff
Pieterjan De Clippel <pieterjandeclippel@msn.com>
2024-06-24 08:55:40 +0200

    Folder hasher

---
+++ FolderHasher/MintPlayer.FolderHasher/FolderHasher.cs
@@ -0,0 +1,38 @@
+﻿using MintPlayer.FolderHasher.Abstractions;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace MintPlayer.FolderHasher;
+
+internal class FolderHasher : IFolderHasher
+{
+    public async Task<string> GetFolderHashAsync(string folder)
+    {
+        // assuming you want to include nested folders
+        var files = Directory.GetFiles(folder, "*", SearchOption.AllDirectories)
+                             .OrderBy(p => p)
+                             .ToList();
+
+        var sha = SHA256.Create();
+
+        for (var i = 0; i < files.Count; i++)
+        {
+            var file = files[i];
+
+            // hash path
+            var relativePath = file.Substring(path.Length + 1);
+            var pathBytes = Encoding.UTF8.GetBytes(relativePath.ToLower());
+            sha.TransformBlock(pathBytes, 0, pathBytes.Length, pathBytes, 0);
+
+            // hash contents
+            var contentBytes = await File.ReadAllBytesAsync(file);
+            if (i == files.Count - 1)
+                sha.TransformFinalBlock(contentBytes, 0, contentBytes.Length);
+            else
+                sha.TransformBlock(contentBytes, 0, contentBytes.Length, contentBytes, 0);
+        }
+
+        return BitConverter.ToString(sha.Hash).Replace("-", "").ToLower();
+    }
+}
