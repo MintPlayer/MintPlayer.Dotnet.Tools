def14b8f3bd7555b16451e85b129e25d90509d9d
Pieterjan De Clippel <pieterjandeclippel@msn.com>
2024-09-07 17:25:34 +0200

    SplitLines ext

---
+++ StringExtensions/MintPlayer.StringExtensions/SplitLines.cs
@@ -0,0 +1,13 @@
+﻿namespace MintPlayer.StringExtensions;
+
+public static class SplitLinesExtensions
+{
+    public static IEnumerable<string> SplitLines(string input, params string[] lineEndings)
+    {
+        if (string.IsNullOrWhiteSpace(input))
+            return Enumerable.Empty<string>();
+
+        var lines = input.Split(lineEndings, StringSplitOptions.None);
+        return lines.Length == 0 ? Enumerable.Empty<string>() : lines;
+    }
+}
