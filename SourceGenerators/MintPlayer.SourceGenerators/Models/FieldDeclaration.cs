6bc477ccba01cacf58bbbeab07ae01d63aab06a5
Pieterjan De Clippel <pieterjandeclippel@msn.com>
2024-09-27 11:54:06 +0200

    DI

---
+++ SourceGenerators/MintPlayer.SourceGenerators/Models/FieldDeclaration.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MintPlayer.SourceGenerators.Models
+{
+    public class FieldDeclaration
+    {
+        public string? Name { get; set; }
+        public string? FullyQualifiedClassName { get; set; }
+        public string? ClassName { get; set; }
+        public string? Namespace { get; set; }
+        public string? FullyQualifiedTypeName { get; set; }
+        public string? Type { get; set; }
+    }
+}
