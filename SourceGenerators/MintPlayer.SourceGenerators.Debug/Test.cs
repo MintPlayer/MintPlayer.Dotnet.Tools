4ecc79f45d4f28372c33fa45a6e86f4903f2a8aa
Pieterjan <pieterjandeclippel@msn.com>
2024-09-28 14:01:35 +0200

    Support non-namespaced files (#31)

---
+++ SourceGenerators/MintPlayer.SourceGenerators.Debug/Test.cs
@@ -0,0 +1,8 @@
+﻿
+using MintPlayer.SourceGenerators.Attributes;
+
+public partial class Class2
+{
+    [Inject] private readonly ITestService3 testService3;
+    [Inject] private readonly ITestService4 testService4;
+}
