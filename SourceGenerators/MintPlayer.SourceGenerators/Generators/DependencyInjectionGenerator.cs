195e94fe907c6d35dbe43fc7b01f0a28babff488
Pieterjan De Clippel <pieterjandeclippel@msn.com>
2024-10-24 18:26:39 +0200

    Temp commit

--- SourceGenerators/MintPlayer.SourceGenerators/Generators/DependencyInjectionGenerator.cs
+++ SourceGenerators/MintPlayer.SourceGenerators/Generators/DependencyInjectionGenerator.cs
@@ -1,7 +1,11 @@
 ﻿using Microsoft.CodeAnalysis;
+using MintPlayer.SourceGenerators.Tools.ValueComparers;
+using MintPlayer.SourceGenerators.Tools;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using MintPlayer.SourceGenerators.Models;
 
 namespace MintPlayer.SourceGenerators.Generators
 {
@@ -14,6 +18,45 @@ namespace MintPlayer.SourceGenerators.Generators
             
         public void Initialize(IncrementalGeneratorInitializationContext context)
         {
+            var config = context.AnalyzerConfigOptionsProvider
+                .Select(static (p, ct) =>
+                {
+                    p.GlobalOptions.TryGetValue("build_property.rootnamespace", out var rootNamespace);
+                    return new Settings
+                    {
+                        RootNamespace = rootNamespace,
+                    };
+                })
+                .WithComparer(SettingsValueComparer.Instance);
+
+            var classesWithRegisterAttributeProvider = context.SyntaxProvider
+                .CreateSyntaxProvider(
+                    static (node, ct) => true,
+                    static (context2, ct) => new Models.ServiceRegistration()
+                )
+                .WithComparer(ValueComparers.ServiceRegistrationComparer.Instance)
+                .Collect();
+
+            //var classesWithRegisterAttributeProvider = context.SyntaxProvider
+            //    .CreateSyntaxProvider(
+            //        static (node, ct) => node is ClassDeclarationSyntax,
+            //        static (context2, ct) =>
+            //        {
+            //            return new ServiceRegistration();
+            //        }
+            //    )
+            //    //.WithComparer(ValueComparers.ServiceRegistrationComparer.Instance)
+            //    .Collect();
+
+            //var registerAttributeSourceProvider = classesWithRegisterAttributeProvider
+            //    .Combine(config)
+            //    .Select(static (p, ct) => new Producers.RegistrationsProducer(p.Left.SelectMany(c => c), p.Right.RootNamespace!));
+
+            //// Combine all source providers
+            //var sourceProvider = registerAttributeSourceProvider;
+
+            //// Generate code
+            //context.RegisterSourceOutput(sourceProvider, static (c, g) => g?.Produce(c));
         }
     }
 }
---
+++ SourceGenerators/MintPlayer.SourceGenerators/Models/ServiceRegistration.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using MintPlayer.SourceGenerators.Tools;
+using MintPlayer.SourceGenerators.ValueComparers;
+
+namespace MintPlayer.SourceGenerators.Models
+{
+    [ValueComparer(typeof(ServiceRegistrationComparer))]
+    public class ServiceRegistration
+    {
+        public string? ServiceTypeName { get; set; }
+        public string? ImplementationTypeName { get; set; }
+        public ServiceLifetime Lifetime { get; set; }
+    }
+}
