c48002f2fd1b3984e9fa09a677d472c8a021e00b
Pieterjan De Clippel <pieterjandeclippel@msn.com>
2024-10-24 17:52:25 +0200

    DI Generator

---
+++ SourceGenerators/MintPlayer.SourceGenerators/Generators/DependencyInjectionGenerator.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.CodeAnalysis;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MintPlayer.SourceGenerators.Generators
+{
+    [Generator(LanguageNames.CSharp)]
+    public class DependencyInjectionGenerator : IIncrementalGenerator
+    {
+        public DependencyInjectionGenerator()
+        {
+        }
+            
+        public void Initialize(IncrementalGeneratorInitializationContext context)
+        {
+        }
+    }
+}
