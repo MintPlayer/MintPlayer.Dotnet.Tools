8006a2193831bce19ac60f9f3d72cfda07a46a3f
Pieterjan De Clippel <pieterjandeclippel@msn.com>
2024-12-23 16:30:08 +0100

    Removed globalsuppressions

--- SourceGenerators/MintPlayer.SourceGenerators/GlobalSuppressions.cs
+++
@@ -1,9 +0,0 @@
-﻿// This file is used by Code Analysis to maintain SuppressMessage
-// attributes that are applied to this project.
-// Project-level suppressions either have no target or are given
-// a specific target and scoped to a namespace, type, member, etc.
-
-using System.Diagnostics.CodeAnalysis;
-
-//[assembly: SuppressMessage("MicrosoftCodeAnalysisCorrectness", "RS1035:Do not use APIs banned for analyzers", Justification = "<Pending>", Scope = "member", Target = "~M:MintPlayer.SourceGenerators.Generators.InjectSourceGenerator.Initialize(Microsoft.CodeAnalysis.GeneratorInitializationContext)")]
-//[assembly: SuppressMessage("MicrosoftCodeAnalysisCorrectness", "RS1035:Do not use APIs banned for analyzers", Justification = "<Pending>", Scope = "member", Target = "~M:MintPlayer.SourceGenerators.Generators.InjectSourceGenerator.Execute(Microsoft.CodeAnalysis.GeneratorExecutionContext)")]
