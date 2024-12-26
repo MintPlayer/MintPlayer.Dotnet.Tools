e74d764763eef36fa44981855c079b7b430dbdbf
Pieterjan De Clippel <pieterjandeclippel@msn.com>
2024-12-26 23:09:22 +0100

    Temp commit

---
+++ SourceGenerators/MintPlayer.SourceGenerators.Tools/IncrementalGenerator.Combine.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MintPlayer.SourceGenerators.Tools
+{
+    public abstract partial class IncrementalGenerator
+    {
+        // SourceGenerators/MintPlayer.SourceGenerators.Tools/Extensions/TypeExtensions.cs
+    }
+}
