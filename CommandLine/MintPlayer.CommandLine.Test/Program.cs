078ebbf6987d44ec2df328e488bc955607fd0e3a
Pieterjan De Clippel <pieterjandeclippel@msn.com>
2024-12-29 17:05:17 +0100

    Add link

--- CommandLine/MintPlayer.CommandLine.Test/Program.cs
+++ CommandLine/MintPlayer.CommandLine.Test/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Microsoft has a new experimental library to build command-line applications
+// Which is described here:
+// https://learn.microsoft.com/en-us/dotnet/standard/commandline/get-started-tutorial#add-a-subcommand-and-options
+// But I don't really like it, so here's my take on it.
