0dafb6b8eb94833846c8020f67803bce25ae4df8
Pieterjan De Clippel <pieterjandeclippel@msn.com>
2024-12-29 17:16:07 +0100

    Required

--- CommandLine/MintPlayer.CommandLine.Test/Commands/Read/ReadSizeCommand.cs
+++ CommandLine/MintPlayer.CommandLine.Test/Commands/Read/ReadSizeCommand.cs
@@ -2,6 +2,7 @@
 
 namespace MintPlayer.CommandLine.Test.Commands.Read;
 
+[CommandOption<Options.FileOption>(Required = true)]
 public class ReadSizeCommand : ICommand
 {
     public string Name => "size";
