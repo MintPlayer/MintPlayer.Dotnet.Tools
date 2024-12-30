a7b64746e768f447c0e16568664b279eb2599762
Pieterjan De Clippel <pieterjandeclippel@msn.com>
2024-12-30 14:33:29 +0100

    Error case

---
+++ SourceGenerators/TestProjects/InterfaceImplementationDebugging/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+public interface IFaceBase
+{
+    public string? Name { get; set; }
+}
+
+public interface IFace
+{
+    //public string? Name { get; set; }
+}
+
+public class Face : IFace
+{
+    public string? Name { get; set; }
+}
