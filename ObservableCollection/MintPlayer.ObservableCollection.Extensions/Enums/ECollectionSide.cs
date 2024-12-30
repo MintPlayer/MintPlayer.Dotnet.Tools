7c4a396ca1b1bce0de4cf980191f85e47d64462d
Tiago Conceição <Tiago_caza@hotmail.com>
2024-12-30 10:02:21 +0000

    Allow MaxItemCount with Insert Method and improve nullable types (#48)
    * Allow MaxItemCount with Insert Method and improve nullable types
    
    * Add comment for Insert
    
    * Use ToArray() as it is more performant
    
    * More generic approach
    
    * Remove remarks on new overload
    
    ---------
    
    Co-authored-by: Pieterjan De Clippel <pieterjandeclippel@msn.com>

---
+++ ObservableCollection/MintPlayer.ObservableCollection.Extensions/Enums/ECollectionSide.cs
@@ -0,0 +1,13 @@
+﻿namespace MintPlayer.ObservableCollection.Extensions.Enums;
+
+/// <summary>
+/// Indicates the head or tail of the collection
+/// </summary>
+public enum ECollectionSide
+{
+    /// <summary>Head/front of the collection</summary>
+    Head,
+
+    /// <summary>Tail/end of the collection</summary>
+    Tail,
+}
--- ObservableCollection/MintPlayer.ObservableCollection.Extensions/ObservableCollectionExtensions.cs
+++ ObservableCollection/MintPlayer.ObservableCollection.Extensions/ObservableCollectionExtensions.cs
@@ -1,17 +1,32 @@
-﻿namespace MintPlayer.ObservableCollection.Extensions;
+﻿using MintPlayer.ObservableCollection.Extensions.Enums;
+
+namespace MintPlayer.ObservableCollection.Extensions;
 
 public static class ObservableCollectionExtensions
 {
     /// <summary>
-    /// Only call this method on Add (when adding items at the end of the collection)
+    /// Ensure the collection have at most <paramref name="maxItemCount"/> items, exceeding items will be removed from the head/tail of the collection.
     /// </summary>
-    private static void RemoveExceeding<T>(this ObservableCollection<T> collection, int maxItemCount)
+    /// <typeparam name="T">Collection type</typeparam>
+    /// <param name="collection">Collection to restrict the number of items for</param>
+    /// <param name="maxItemCount">The maximum number of items to keep in this <paramref name="collection"/>.</param>
+    private static void RemoveExceedingAt<T>(this ObservableCollection<T> collection, int maxItemCount, ECollectionSide side)
     {
         ArgumentOutOfRangeException.ThrowIfNegativeOrZero(maxItemCount);
 
         var exceed = collection.Count - maxItemCount;
         if (exceed > 0)
-            collection.RemoveRange(0, exceed);
+        {
+            switch (side)
+            {
+                case ECollectionSide.Head:
+                    collection.RemoveRange(0, exceed);
+                    break;
+                case ECollectionSide.Tail:
+                    collection.RemoveRange(maxItemCount, exceed);
+                    break;
+            }
+        }
     }
 
     public static void AddRange<T>(this ObservableCollection<T> collection, System.Collections.IEnumerable items)
@@ -22,19 +37,19 @@ public static class ObservableCollectionExtensions
     public static void AddRange<T>(this ObservableCollection<T> collection, System.Collections.IEnumerable items, int maxItemCount)
     {
         collection.AddRange(items.Cast<T>());
-        collection.RemoveExceeding(maxItemCount);
+        collection.RemoveExceedingAt(maxItemCount, ECollectionSide.Head);
     }
 
     public static void AddRange<T>(this ObservableCollection<T> collection, IEnumerable<T> items, int maxItemCount)
     {
         collection.AddRange(items);
-        collection.RemoveExceeding(maxItemCount);
+        collection.RemoveExceedingAt(maxItemCount, ECollectionSide.Head);
     }
 
     public static void Add<T>(this ObservableCollection<T> collection, T item, int maxItemCount)
     {
         collection.Add(item);
-        collection.RemoveExceeding(maxItemCount);
+        collection.RemoveExceedingAt(maxItemCount, ECollectionSide.Head);
     }
 
     public static void RemoveRange<T>(this ObservableCollection<T> collection, System.Collections.IEnumerable items)
@@ -44,11 +59,46 @@ public static class ObservableCollectionExtensions
 
     public static void RemoveRange<T>(this ObservableCollection<T> collection, int start, int count)
     {
-        if (start < 0) throw new ArgumentOutOfRangeException(nameof(start), start, "Index can not be negative.");
-        if (count < 0) throw new ArgumentOutOfRangeException(nameof(count), count, "Count can not be negative.");
+        ArgumentOutOfRangeException.ThrowIfNegative(start);
+        ArgumentOutOfRangeException.ThrowIfNegative(count);
         if (count == 0) return;
         if (start >= collection.Count) throw new ArgumentOutOfRangeException(nameof(start), start, "The index is outside the range of this list.");
 
         collection.RemoveRange(collection.Skip(start).Take(count));
     }
+
+    /// <summary>
+    /// Insert an item at the specified index, and ensure the collection have at most <paramref name="maxItemCount"/> items.
+    /// </summary>
+    /// <remarks>
+    /// If the <paramref name="index"/> is greater than or equal to the half of the collection count, exceeding items will be removed from the head of the collection, otherwise at the tail.
+    /// </remarks>
+    /// <typeparam name="T">Collection type</typeparam>
+    /// <param name="collection">Collection to restrict the number of items for</param>
+    /// <param name="index">Position at where the item should be inserted</param>
+    /// <param name="item">Item to insert</param>
+    /// <param name="maxItemCount">The maximum number of items to keep in this <paramref name="collection"/>.</param>
+    public static void Insert<T>(this ObservableCollection<T> collection, int index, T item, int maxItemCount)
+    {
+        collection.Insert(index, item);
+        var halfCount = (collection.Count - 1) / 2;
+        if (index >= halfCount) collection.RemoveExceedingAt(maxItemCount, ECollectionSide.Head);
+        else collection.RemoveExceedingAt(maxItemCount, ECollectionSide.Tail);
+    }
+
+    /// <summary>
+    /// Insert an item at the specified index, and ensure the collection have at most <paramref name="maxItemCount"/> items.
+    /// </summary>
+    /// <typeparam name="T">Collection type</typeparam>
+    /// <param name="collection">Collection to restrict the number of items for</param>
+    /// <param name="index">Position at where the item should be inserted</param>
+    /// <param name="item">Item to insert</param>
+    /// <param name="maxItemCount">The maximum number of items to keep in this <paramref name="collection"/>.</param>
+    /// <param name="removeItemsAt">Whether to remove excessive items at the start/end of the collection</param>
+    public static void Insert<T>(this ObservableCollection<T> collection, int index, T item, int maxItemCount, ECollectionSide removeItemsAt)
+    {
+        collection.Insert(index, item);
+        collection.RemoveExceedingAt(maxItemCount, removeItemsAt);
+    }
 }
+
