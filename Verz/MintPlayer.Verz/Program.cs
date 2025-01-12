b386de409ea7bc304917c35224537e06229d5223
PieterjanDeClippel <pieterjandeclippel@msn.com>
2025-01-12 17:44:32 +0100

    Ship a test tool

--- Verz/MintPlayer.Verz/Program.cs
+++ Verz/MintPlayer.Verz/Program.cs
@@ -1,2 +1,27 @@
 ﻿// dotnet tool install --global MintPlayer.Verz
-Console.WriteLine("Hello, World!");
+
+using System.CommandLine;
+
+namespace MintPlayer.Verz;
+
+class Program
+{
+    static async Task<int> Main(string[] args)
+    {
+        var fileOption = new Option<FileInfo?>(
+            name: "--file",
+            description: "The file to read and display on the console.");
+
+        var rootCommand = new RootCommand("Sample app for System.CommandLine");
+        rootCommand.AddOption(fileOption);
+        rootCommand.SetHandler(file => ReadFile(file!), fileOption);
+
+        return await rootCommand.InvokeAsync(args);
+    }
+
+    static void ReadFile(FileInfo file)
+    {
+        File.ReadLines(file.FullName).ToList()
+            .ForEach(line => Console.WriteLine(line));
+    }
+}
