2e4a3bc3c334e24a434cbb43b34607b5b5dc34de
PieterjanDeClippel <pieterjandeclippel@msn.com>
2025-01-12 22:17:13 +0100

    Creating a cli-tool

---
+++ Verz/MintPlayer.Verz/VerzConfig.cs
@@ -0,0 +1,18 @@
+﻿namespace MintPlayer.Verz
+{
+    public class VerzConfig
+    {
+        public VerzFeed[] Feeds { get; set; } = [];
+        public VerzSdk[] Sdks { get; set; } = [];
+    }
+
+    public class VerzFeed
+    {
+        public string? Url { get; set; }
+    }
+
+    public class VerzSdk
+    {
+
+    }
+}
