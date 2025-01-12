5eda6a244f48cb94b7cea838a701e1820123a583
PieterjanDeClippel <pieterjandeclippel@msn.com>
2025-01-12 22:17:13 +0100

    Creating a cli-tool

--- Verz/MintPlayer.Verz/Program.cs
+++ Verz/MintPlayer.Verz/Program.cs
@@ -1,6 +1,7 @@
 ﻿// dotnet tool install --global MintPlayer.Verz
 
 using System.CommandLine;
+using System.Reflection;
 
 namespace MintPlayer.Verz;
 
@@ -16,8 +17,48 @@ class Program
         rootCommand.AddOption(fileOption);
         rootCommand.SetHandler(file => ReadFile(file!), fileOption);
 
+        var packagesCommand = new Command("packages", "Manage globally installed nuget packages");
+        rootCommand.AddCommand(packagesCommand);
+
+        var listPackagesCommand = new Command("list", "Lists all globally installed nuget packages");
+        packagesCommand.AddCommand(listPackagesCommand);
+        listPackagesCommand.SetHandler(async () =>
+        {
+
+
+            var globalPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), ".nuget", "packages");
+            var modules = Directory.GetDirectories(globalPath);
+            var moduleAssemblies = await Task.WhenAll(modules.Select(m => AnalyzeModule(m)));
+            var validModules = moduleAssemblies.Where(asm => asm is not null && asm.IsFullyTrusted).ToList();
+        });
+
         var statusCode = await rootCommand.InvokeAsync(args);
         return statusCode;
+    }
+
+    private static async Task<Assembly?> AnalyzeModule(string modulePath)
+    {
+        await Task.CompletedTask;
+
+        var firstVersion = Directory.EnumerateDirectories(modulePath).FirstOrDefault();
+        if (firstVersion == null) return null;
+
+        var libFolder = Path.Combine(firstVersion, "lib");
+        if (!Directory.Exists(libFolder)) return null;
+
+        var moduleName = Path.GetFileName(modulePath);
+        var entryPoint = Directory.EnumerateFiles(libFolder, $"{moduleName}.dll", SearchOption.AllDirectories).LastOrDefault();
+        if (entryPoint == null) return null;
+
+        try
+        {
+            var asm = Assembly.LoadFrom(entryPoint);
+            return asm;
+        }
+        catch (Exception)
+        {
+            return null;
+        }
     }
 
     static async Task ReadFile(FileInfo file)
