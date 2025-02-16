1aa04d60925fbaa0aec959012b99cf8028e88561
PieterjanDeClippel <pieterjandeclippel@msn.com>
2025-02-16 09:48:26 +0100

    Temp commit

--- Beid/DemoWebApp/Program.cs
+++ Beid/DemoWebApp/Program.cs
@@ -18,50 +18,50 @@ public class Program
 
         var builder = WebApplication.CreateSlimBuilder(args);
 
-        builder.Services.Configure<EidAuthenticationOptions>(options =>
-        {
-            options.SignInAsAuthenticationType = "Cookies";
-        });
+        //builder.Services.Configure<EidAuthenticationOptions>(options =>
+        //{
+        //    options.SignInAsAuthenticationType = "Cookies";
+        //});
 
-        builder.Services.ConfigureHttpJsonOptions(options =>
-        {
-            options.SerializerOptions.TypeInfoResolverChain.Insert(0, AppJsonSerializerContext.Default);
-        });
+        //builder.Services.ConfigureHttpJsonOptions(options =>
+        //{
+        //    options.SerializerOptions.TypeInfoResolverChain.Insert(0, AppJsonSerializerContext.Default);
+        //});
 
-        builder.Services.AddAuthentication(CertificateAuthenticationDefaults.AuthenticationScheme)//("Eid")
-            .AddCertificate(o =>
-            {
-                //o.RevocationMode = System.Security.Cryptography.X509Certificates.X509RevocationMode.NoCheck;
-                o.AllowedCertificateTypes = CertificateTypes.All;
-                //o.ValidateCertificateUse = false;
-                //o.ValidateValidityPeriod = false;
-                //o.ChainTrustValidationMode = System.Security.Cryptography.X509Certificates.X509ChainTrustMode.System;
-                //o.ChainTrustValidationMode = System.Security.Cryptography.X509Certificates.X509ChainTrustMode.CustomRootTrust;
-                o.RevocationMode = System.Security.Cryptography.X509Certificates.X509RevocationMode.NoCheck;
-                //o.CustomTrustStore = new System.Security.Cryptography.X509Certificates.X509Certificate2Collection
-                //{
+        //builder.Services.AddAuthentication(CertificateAuthenticationDefaults.AuthenticationScheme)//("Eid")
+        //    .AddCertificate(o =>
+        //    {
+        //        //o.RevocationMode = System.Security.Cryptography.X509Certificates.X509RevocationMode.NoCheck;
+        //        o.AllowedCertificateTypes = CertificateTypes.All;
+        //        //o.ValidateCertificateUse = false;
+        //        //o.ValidateValidityPeriod = false;
+        //        //o.ChainTrustValidationMode = System.Security.Cryptography.X509Certificates.X509ChainTrustMode.System;
+        //        //o.ChainTrustValidationMode = System.Security.Cryptography.X509Certificates.X509ChainTrustMode.CustomRootTrust;
+        //        o.RevocationMode = System.Security.Cryptography.X509Certificates.X509RevocationMode.NoCheck;
+        //        //o.CustomTrustStore = new System.Security.Cryptography.X509Certificates.X509Certificate2Collection
+        //        //{
 
-                //};
+        //        //};
 
-                o.Events = new CertificateAuthenticationEvents
-                {
-                    OnChallenge = async (context) =>
-                    {
+        //        o.Events = new CertificateAuthenticationEvents
+        //        {
+        //            OnChallenge = async (context) =>
+        //            {
 
-                    },
-                    OnAuthenticationFailed = async (context) =>
-                    {
+        //            },
+        //            OnAuthenticationFailed = async (context) =>
+        //            {
 
-                    },
-                    OnCertificateValidated = (context) =>
-                    {
-                        //var thb = context.ClientCertificate?.Thumbprint;
-                        //context.Principal = new ClaimsPrincipal(new ClaimsIdentity([], context.Scheme.Name));
-                        context.Success();
-                        return Task.CompletedTask;
-                    }
-                };
-            });
+        //            },
+        //            OnCertificateValidated = (context) =>
+        //            {
+        //                //var thb = context.ClientCertificate?.Thumbprint;
+        //                //context.Principal = new ClaimsPrincipal(new ClaimsIdentity([], context.Scheme.Name));
+        //                context.Success();
+        //                return Task.CompletedTask;
+        //            }
+        //        };
+        //    });
 
         //builder.Services.AddAuthorization(o =>
         //{
@@ -73,13 +73,13 @@ public class Program
 
         builder.Services.AddControllersWithViews().AddNewtonsoftJson();
 
-        builder.Services.AddDbContext<EidContext>();
+        //builder.Services.AddDbContext<EidContext>();
 
-        builder.Services.AddIdentity<User, Role>()
-            .AddEntityFrameworkStores<EidContext>()
-            .AddDefaultTokenProviders();
+        //builder.Services.AddIdentity<User, Role>()
+        //    .AddEntityFrameworkStores<EidContext>()
+        //    .AddDefaultTokenProviders();
 
-        builder.Services.AddHealthChecks();
+        //builder.Services.AddHealthChecks();
 
         builder.WebHost.ConfigureKestrel(k =>
         {
@@ -99,24 +99,24 @@ public class Program
 
         var app = builder.Build();
 
-        var sampleTodos = new Todo[] {
-            new(1, "Walk the dog"),
-            new(2, "Do the dishes", DateOnly.FromDateTime(DateTime.Now)),
-            new(3, "Do the laundry", DateOnly.FromDateTime(DateTime.Now.AddDays(1))),
-            new(4, "Clean the bathroom"),
-            new(5, "Clean the car", DateOnly.FromDateTime(DateTime.Now.AddDays(2)))
-        };
+        //var sampleTodos = new Todo[] {
+        //    new(1, "Walk the dog"),
+        //    new(2, "Do the dishes", DateOnly.FromDateTime(DateTime.Now)),
+        //    new(3, "Do the laundry", DateOnly.FromDateTime(DateTime.Now.AddDays(1))),
+        //    new(4, "Clean the bathroom"),
+        //    new(5, "Clean the car", DateOnly.FromDateTime(DateTime.Now.AddDays(2)))
+        //};
 
         //app.UseExceptionHandler(@"/Test.txt");
-        app.UseHealthChecks("/healtz");
+        //app.UseHealthChecks("/healtz");
         app.UseDeveloperExceptionPage();
         app.UseHttpsRedirection();
-        app.UseStatusCodePages();
+        //app.UseStatusCodePages();
         app.UseDeveloperExceptionPage();
 
-        app.UseAuthentication();
+        //app.UseAuthentication();
         app.UseRouting();
-        app.UseAuthorization();
+        //app.UseAuthorization();
         app.UseEndpoints(endpoints =>
         {
             endpoints.MapControllers();
@@ -124,7 +124,7 @@ public class Program
 
         //app.UseEidAuthentication();
 
-        app.MapGet("/", () => "Hello");
+        //app.MapGet("/", () => "Hello");
 
         var todosApi = app.MapGroup("/todos");
         todosApi.MapGet("/", async (c) =>
@@ -133,43 +133,43 @@ public class Program
             var result = new PersonInfo(cert!);
             await c.Response.WriteAsJsonAsync(result);
         });
-        todosApi.MapGet("/{id}", (int id) =>
-            sampleTodos.FirstOrDefault(a => a.Id == id) is { } todo
-                ? Results.Ok(todo)
-                : Results.NotFound());
+        //todosApi.MapGet("/{id}", (int id) =>
+        //    sampleTodos.FirstOrDefault(a => a.Id == id) is { } todo
+        //        ? Results.Ok(todo)
+        //        : Results.NotFound());
 
-        app.MapGet("/cert", () =>
-        {
-            // https://learn.microsoft.com/en-us/aspnet/core/security/authentication/certauth?view=aspnetcore-8.0#create-certificates-in-powershell
-            // $rootpassword = ConvertTo-SecureString -String "1234" -Force -AsPlainText
-            // $rootcert = New-SelfSignedCertificate -Type Custom -KeySpec Signature -Subject "CN=P2SRootCert" -KeyExportPolicy Exportable -HashAlgorithm sha256 -KeyLength 4096 -CertStoreLocation "Cert:\CurrentUser\My" -KeyUsageProperty Sign -KeyUsage CertSign -NotAfter(Get-Date).AddYears(5)
-            // $rootcertpath = 'Cert:\CurrentUser\My\' + $rootcert.Thumbprint
-            // $childcert = New-SelfSignedCertificate -Type Custom -KeySpec Signature -Subject "CN=P2SChildCert" -KeyExportPolicy Exportable -HashAlgorithm sha256 -KeyLength 2048 -NotAfter(Get-Date).AddMonths(24) -CertStoreLocation "Cert:\CurrentUser\My" -Signer $rootcert -TextExtension @("2.5.29.37={text}1.3.6.1.5.5.7.3.2")
-            // $childcertpath = 'Cert:\CurrentUser\My\' + $childcert.Thumbprint
-            // Export-Certificate -Cert $rootcertpath -FilePath root_example.crt -Password $rootpassword
-            // Export-Certificate -Cert $childcertpath -FilePath child_example.crt
-
-
-            // Also todo:
-            // Download + install EID viewer
-            // Insert card => Under Certificates tab => Right-click Belgium Root CA4 => Gedetailleerde informatie
-            // => Certificaat installeren
+        //app.MapGet("/cert", () =>
+        //{
+        //    // https://learn.microsoft.com/en-us/aspnet/core/security/authentication/certauth?view=aspnetcore-8.0#create-certificates-in-powershell
+        //    // $rootpassword = ConvertTo-SecureString -String "1234" -Force -AsPlainText
+        //    // $rootcert = New-SelfSignedCertificate -Type Custom -KeySpec Signature -Subject "CN=P2SRootCert" -KeyExportPolicy Exportable -HashAlgorithm sha256 -KeyLength 4096 -CertStoreLocation "Cert:\CurrentUser\My" -KeyUsageProperty Sign -KeyUsage CertSign -NotAfter(Get-Date).AddYears(5)
+        //    // $rootcertpath = 'Cert:\CurrentUser\My\' + $rootcert.Thumbprint
+        //    // $childcert = New-SelfSignedCertificate -Type Custom -KeySpec Signature -Subject "CN=P2SChildCert" -KeyExportPolicy Exportable -HashAlgorithm sha256 -KeyLength 2048 -NotAfter(Get-Date).AddMonths(24) -CertStoreLocation "Cert:\CurrentUser\My" -Signer $rootcert -TextExtension @("2.5.29.37={text}1.3.6.1.5.5.7.3.2")
+        //    // $childcertpath = 'Cert:\CurrentUser\My\' + $childcert.Thumbprint
+        //    // Export-Certificate -Cert $rootcertpath -FilePath root_example.crt -Password $rootpassword
+        //    // Export-Certificate -Cert $childcertpath -FilePath child_example.crt
 
 
-            var properties = new AuthenticationProperties { RedirectUri = "/cert/callback" };
-            return Results.Challenge(properties, [CertificateAuthenticationDefaults.AuthenticationScheme]);
-        }).AllowAnonymous();
+        //    // Also todo:
+        //    // Download + install EID viewer
+        //    // Insert card => Under Certificates tab => Right-click Belgium Root CA4 => Gedetailleerde informatie
+        //    // => Certificaat installeren
 
-        app.MapGet("/login", (SignInManager<User> signInManager) =>
-        {
-            var properties = signInManager.ConfigureExternalAuthenticationProperties(CertificateAuthenticationDefaults.AuthenticationScheme, "/login/callback");
-            return Results.Challenge(properties, [CertificateAuthenticationDefaults.AuthenticationScheme]);
-        });
 
-        app.MapGet("/login/callback", () =>
-        {
-            return Results.Ok();
-        });
+        //    var properties = new AuthenticationProperties { RedirectUri = "/cert/callback" };
+        //    return Results.Challenge(properties, [CertificateAuthenticationDefaults.AuthenticationScheme]);
+        //}).AllowAnonymous();
+
+        //app.MapGet("/login", (SignInManager<User> signInManager) =>
+        //{
+        //    var properties = signInManager.ConfigureExternalAuthenticationProperties(CertificateAuthenticationDefaults.AuthenticationScheme, "/login/callback");
+        //    return Results.Challenge(properties, [CertificateAuthenticationDefaults.AuthenticationScheme]);
+        //});
+
+        //app.MapGet("/login/callback", () =>
+        //{
+        //    return Results.Ok();
+        //});
 
         app.Run();
     }
