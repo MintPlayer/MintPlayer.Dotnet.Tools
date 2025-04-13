4bb80d5a54e489c7a2d7d8d41bdd577de83346f2
Pieterjan <pieterjandeclippel@msn.com>
2025-04-13 11:06:14 +0200

    Newtonsoft json value comparer (#79)
    * Add Newtonsoft.Json value comparer project
    
    * Add filenesting.targets
    
    * Versions

---
+++ SourceGenerators/MintPlayer.SourceGenerators.Tools/ValueComparers/SourceTextValueComparer.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.CodeAnalysis.Text;
+
+namespace MintPlayer.SourceGenerators.Tools.ValueComparers;
+
+/// <summary>
+/// Value Comparer for SourceText
+/// </summary>
+public sealed class SourceTextValueComparer : ValueComparer<SourceText>
+{
+    protected override bool AreEqual(SourceText x, SourceText y)
+    {
+        return x.ContentEquals(y);
+    }
+}
---
+++ SourceGenerators/ValueComparers/MintPlayer.ValueComparers.NewtonsoftJson/RegisterNewtonsoftJsonComparers.cs
@@ -0,0 +1,16 @@
+﻿using MintPlayer.SourceGenerators.Tools.ValueComparers;
+using Newtonsoft.Json.Linq;
+
+namespace MintPlayer.ValueComparers.NewtonsoftJson;
+
+public static class NewtonsoftJsonComparers
+{
+    private static bool isRegistered = false;
+    public static void Register()
+    {
+        if (isRegistered) return;
+        isRegistered = true;
+
+        ValueComparer<JObject>.RegisterCustomComparer<JObjectValueComparer>();
+    }
+}
