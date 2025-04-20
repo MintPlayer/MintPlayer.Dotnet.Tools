022ab00623da4f140a787c7b3e13d5867c5a0352
PieterjanDeClippel <pieterjandeclippel@msn.com>
2025-04-20 17:34:10 +0200

    Attempt to combine providers

---
+++ SourceGenerators/SourceGenerators/MintPlayer.SourceGenerators/Exte.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.CodeAnalysis;
+
+namespace MintPlayer.SourceGenerators
+{
+    public static class StaticMethods
+    {
+        public static IncrementalValuesProvider<string> Combine<T1, T2, T3>((IncrementalValueProvider<T1>, IncrementalValueProvider<T2>, IncrementalValueProvider<T3>) tuple)
+        {
+            return tuple.Item1
+                .Combine(tuple.Item2)
+                .SelectMany(static (p, _) => new object[] { p.Left, p.Right })
+                .Combine(tuple.Item3)
+                .SelectMany(static (p, _) => new object[] { p.Left, p.Right })
+                .Select(static (p, ct) =>
+                {
+                    return "";
+                    // Do something with p.Item1, p.Item2, and p.Item3
+                });
+        }
+    }
+
+}
--- SourceGenerators/SourceGenerators/MintPlayer.SourceGenerators/Generators/ClassNamesSourceGenerator.cs
+++ SourceGenerators/SourceGenerators/MintPlayer.SourceGenerators/Generators/ClassNamesSourceGenerator.cs
@@ -1,8 +1,12 @@
 ﻿using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.Extensions.DependencyInjection;
+using MintPlayer.SourceGenerators.Attributes;
+using MintPlayer.SourceGenerators.Models;
 using MintPlayer.SourceGenerators.Tools;
 using MintPlayer.ValueComparers.NewtonsoftJson;
+
 
 namespace MintPlayer.SourceGenerators.Generators;
 
@@ -82,6 +86,48 @@ public class ClassNamesSourceGenerator : IncrementalGenerator
         var classNameListSourceProvider = classDeclarationsProvider
             .Combine(settingsProvider)
             .Select(static Producer (p, ct) => new Producers.ClassNameListProducer(declarations: p.Left.NotNull(), rootNamespace: p.Right.RootNamespace!));
+
+        var classesWithRegisterAttributeProvider = context.SyntaxProvider
+            .CreateSyntaxProvider(
+                static (node, ct) => node is ClassDeclarationSyntax,
+                static (context2, ct) =>
+                {
+                    if (context2.Node is ClassDeclarationSyntax classDeclaration)
+                    {
+                        var classSymbol = context2.SemanticModel.GetDeclaredSymbol(classDeclaration, ct);
+                        if (classSymbol is INamedTypeSymbol namedTypeSymbol)
+                        {
+                            var attr = classSymbol.GetAttributes()
+                                .FirstOrDefault(a => a.AttributeClass?.Name == nameof(RegisterAttribute));
+
+                            if (attr is null) return default;
+
+                            if (attr.ConstructorArguments[0].Value is not INamedTypeSymbol interfaceSymbol) return default;
+
+                            // Verify that the class implements the interface
+                            if (namedTypeSymbol.AllInterfaces.All(i => !SymbolEqualityComparer.Default.Equals(i, interfaceSymbol))) return default;
+
+                            return new ServiceRegistration
+                            {
+                                ServiceTypeName = interfaceSymbol.ToDisplayString(SymbolDisplayFormat.FullyQualifiedFormat),
+                                ImplementationTypeName = classSymbol.ToDisplayString(SymbolDisplayFormat.FullyQualifiedFormat),
+                                Lifetime = (ServiceLifetime)attr.ConstructorArguments[1].Value!,
+                                MethodNameHint = (string?)attr.ConstructorArguments[2].Value ?? string.Empty,
+                            };
+                        }
+                    }
+
+                    return default;
+                }
+            )
+            .WithComparer(ServiceRegistrationValueComparer.Instance)
+            .Collect();
+
+        Combine((classDeclarationsProvider, fieldDeclarationsProvider, classesWithRegisterAttributeProvider))
+            .Select((classDeclarations, fieldDeclarations, classesWithRegisterAttribute) =>
+            {
+                return "";
+            });
 
         var classNamesDiagnosticProvider = classDeclarationsProvider
             .Combine(settingsProvider)
