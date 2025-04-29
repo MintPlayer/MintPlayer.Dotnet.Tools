38809baf70ddb0e8fccd0a9b63d139d3fae9ec6c
PieterjanDeClippel <pieterjandeclippel@msn.com>
2025-04-29 14:34:32 +0200

    Add obj/bin remover

---
+++ Helpers/RemoveObjBin/Program.cs
@@ -0,0 +1,15 @@
+﻿var rootPath = Directory.GetCurrentDirectory();
+Console.WriteLine("Current Directory: " + rootPath);
+
+foreach (var binDir in Directory.EnumerateDirectories(rootPath, "bin|obj|bin-windows|bin-linux|obj-windows|obj-linux|tmp-build", SearchOption.AllDirectories))
+{
+    try
+    {
+        Console.WriteLine($"Deleting: {binDir}");
+        Directory.Delete(binDir, recursive: true);
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Failed to delete {binDir}: {ex.Message}");
+    }
+}
