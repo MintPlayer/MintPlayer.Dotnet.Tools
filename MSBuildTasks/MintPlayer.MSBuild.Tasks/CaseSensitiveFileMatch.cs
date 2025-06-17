729345ab941920b76f87a8e19030eb9bd388fd51
Pieterjan <pieterjandeclippel@msn.com>
2025-06-17 09:05:51 +0200

    Add msbuild tasks library (#84)
    * Add msbuild tasks library
    
    * Test

---
+++ MSBuildTasks/MintPlayer.MSBuild.Tasks/CaseSensitiveFileMatch.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Build.Framework;
+using System.IO;
+using System.Linq;
+
+namespace MintPlayer.MSBuild.Tasks
+{
+    public class CaseSensitiveFileMatch : Microsoft.Build.Utilities.Task
+    {
+        [Required]
+        public string Directory { get; set; }
+
+        [Required]
+        public string Candidate { get; set; }
+
+        [Output]
+        public bool Exists { get; set; }
+
+        public override bool Execute()
+        {
+            Exists = System.IO.Directory.EnumerateFiles(Directory, Path.GetFileName(Candidate))
+                .Any(file => string.Equals(Path.GetFileName(file), Path.GetFileName(Candidate), System.StringComparison.Ordinal));
+
+            return true;
+        }
+    }
+}
+
