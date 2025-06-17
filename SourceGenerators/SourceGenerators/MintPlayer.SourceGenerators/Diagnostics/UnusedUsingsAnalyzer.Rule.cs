89535b0bdd5eb6e7a502df7665365129eadff98c
PieterjanDeClippel <pieterjandeclippel@msn.com>
2025-06-17 08:43:32 +0200

    Temp commit

---
+++ SourceGenerators/SourceGenerators/MintPlayer.SourceGenerators/Diagnostics/UnusedUsingsAnalyzer.Rule.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.CodeAnalysis;
+
+namespace MintPlayer.SourceGenerators.Diagnostics;
+
+public static partial class DiagnosticRules
+{
+    public static readonly DiagnosticDescriptor UnusedUsingsRule = new DiagnosticDescriptor(
+        "RemoveUnusedUsings",
+        "Unused using directive",
+        "Using directive is unused",
+        "Cleanup",
+        DiagnosticSeverity.Info,
+        isEnabledByDefault: true
+    );
+}
---
+++ SourceGenerators/SourceGenerators/MintPlayer.SourceGenerators/Diagnostics/UnusedUsingsAnalyzer.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+using System.Collections.Immutable;
+using System.Data;
+
+namespace MintPlayer.SourceGenerators.Diagnostics;
+
+[DiagnosticAnalyzer(LanguageNames.CSharp)]
+public class UnusedUsingsAnalyzer : DiagnosticAnalyzer
+{
+    public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(DiagnosticRules.UnusedUsingsRule);
+
+    public override void Initialize(AnalysisContext context)
+    {
+        context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
+        context.EnableConcurrentExecution();
+
+        context.RegisterCompilationStartAction(compilationContext =>
+        {
+            compilationContext.RegisterSyntaxTreeAction(syntaxTreeContext =>
+            {
+                var semanticModel = compilationContext.Compilation.GetSemanticModel(syntaxTreeContext.Tree);
+                var diagnostics = semanticModel.GetDiagnostics();
+
+                foreach (var diagnostic in diagnostics)
+                {
+                    if (diagnostic.Id == "CS8019")
+                    {
+                        var root = syntaxTreeContext.Tree.GetRoot(syntaxTreeContext.CancellationToken);
+                        var node = root.FindNode(diagnostic.Location.SourceSpan);
+                        syntaxTreeContext.ReportDiagnostic(Diagnostic.Create(Rule, node.GetLocation()));
+                    }
+                }
+            });
+        });
+    }
+}
