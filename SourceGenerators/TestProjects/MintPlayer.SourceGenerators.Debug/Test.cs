637269e336bf174f88edd463ba33a9330ff1bbee
PieterjanDeClippel <pieterjandeclippel@msn.com>
2025-06-22 15:44:56 +0200

    Fix

--- SourceGenerators/TestProjects/MintPlayer.SourceGenerators.Debug/Test.cs
+++ SourceGenerators/TestProjects/MintPlayer.SourceGenerators.Debug/Test.cs
@@ -3,37 +3,37 @@ using MintPlayer.SourceGenerators.Attributes;
 
 public partial class BaseClass
 {
-    //private readonly string code;
-    //private readonly ITestService1 testService1;
-    //public BaseClass(string code, ITestService1 testService1)
-    //{
-    //    this.code = code;
-    //    this.testService1 = testService1;
-    //}
+    private readonly string code;
+    private readonly ITestService1 testService1;
+    public BaseClass(string code, ITestService1 testService1)
+    {
+        this.code = code;
+        this.testService1 = testService1;
+    }
 }
 
 [BaseConstructorParameter<string>("code", "FR")]
 public partial class Class1 : BaseClass
 {
-    //[Inject] private readonly ITestService1 testService1;
-    //[Inject] private readonly ITestService2 testService2;
+    [Inject] private readonly ITestService1 testService1;
+    [Inject] private readonly ITestService2 testService2;
 }
 
 public partial class Class2
 {
-    //[Inject] private readonly ITestService3 testService3;
-    //[Inject] private readonly ITestService4 testService4;
+    [Inject] private readonly ITestService3 testService3;
+    [Inject] private readonly ITestService4 testService4;
 
-    //void Test()
-    //{
-    //    var message = testService3.GetMessage();
-    //}
+    void Test()
+    {
+        var message = testService3.GetMessage();
+    }
 }
 
 public partial class Class3 : Class1
 {
-    //[Inject] private readonly ITestService3 testService3;
-    //[Inject] private readonly ITestService4 testService4;
+    [Inject] private readonly ITestService3 testService3;
+    [Inject] private readonly ITestService4 testService4;
     void a()
     {
         new ServiceCollection().AddScoped<ITestService1, TestService1>();
