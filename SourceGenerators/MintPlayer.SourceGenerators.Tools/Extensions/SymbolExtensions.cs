0ff3ccdd19de30b269e75ac104b27d43340daa59
Pieterjan <pieterjandeclippel@msn.com>
2025-07-11 14:57:56 +0200

    Fixed the valuecomparer generator (#91)

---
+++ SourceGenerators/MintPlayer.SourceGenerators.Tools/Extensions/SymbolExtensions.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.CodeAnalysis;
+
+namespace MintPlayer.SourceGenerators.Tools.Extensions;
+
+public static class SymbolExtensions
+{
+    public static IEnumerable<IPropertySymbol> GetAllProperties(this INamedTypeSymbol? typeSymbol)
+    {
+        if (typeSymbol is null) return [];
+
+        return typeSymbol.GetAllBaseTypes()
+            .SelectMany(t => t.GetMembers().OfType<IPropertySymbol>());
+    }
+
+    public static IEnumerable<INamedTypeSymbol> GetAllBaseTypes(this INamedTypeSymbol? typeSymbol)
+    {
+        while (typeSymbol is { })
+        {
+            yield return typeSymbol;
+            typeSymbol = typeSymbol.BaseType;
+        }
+    }
+}
