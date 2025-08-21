3700c7ff3fdfabc08669825ef54faed31243947c
PieterjanDeClippel <pieterjandeclippel@msn.com>
2025-08-22 00:31:09 +0200

    IEnumerable

--- SourceGenerators/Mapper/MintPlayer.Mapper/Generators/MapperGenerator.Producer.cs
+++ SourceGenerators/Mapper/MintPlayer.Mapper/Generators/MapperGenerator.Producer.cs
@@ -14,6 +14,81 @@ public sealed class MapperProducer : Producer
 
     protected override void ProduceSource(IndentedTextWriter writer, CancellationToken cancellationToken)
     {
-        throw new NotImplementedException();
+        foreach (var type in typesToMap)
+        {
+            if (!string.IsNullOrEmpty(type.DestinationNamespace))
+            {
+                writer.WriteLine($"namespace {type.DestinationNamespace}");
+                writer.WriteLine("{");
+                writer.Indent++;
+            }
+
+            writer.WriteLine($"public sealed class {type.DeclaredTypeName}Mapper");
+            writer.WriteLine("{");
+            writer.Indent++;
+
+
+            writer.Indent--;
+            writer.WriteLine("}");
+
+            if (!string.IsNullOrEmpty(type.DestinationNamespace))
+            {
+                writer.Indent--;
+                writer.WriteLine("}");
+            }
+        }
+
+        writer.WriteLine($"namespace {RootNamespace}");
+        writer.WriteLine("{");
+        writer.Indent++;
+
+        writer.WriteLine("public static class MapperExtensions");
+        writer.WriteLine("{");
+        writer.Indent++;
+
+        foreach (var type in typesToMap)
+        {
+            writer.WriteLine($"public static {type.DeclaredType} MapTo(this {type.MappingType} input)");
+            writer.WriteLine("{");
+            writer.Indent++;
+
+            writer.WriteLine($"return new {type.DeclaredType}();");
+
+            writer.Indent--;
+            writer.WriteLine("}");
+
+            writer.WriteLine($"public static {type.MappingType} MapTo(this {type.DeclaredType} input)");
+            writer.WriteLine("{");
+            writer.Indent++;
+
+            writer.WriteLine($"return new {type.MappingType}();");
+
+            writer.Indent--;
+            writer.WriteLine("}");
+
+            writer.WriteLine($"public static global::System.Collections.Generic.IEnumerable<{type.DeclaredType}> MapTo(this global::System.Collections.Generic.IEnumerable<{type.MappingType}> input)");
+            writer.WriteLine("{");
+            writer.Indent++;
+
+            writer.WriteLine($"return input.Select(x => x.MapTo());");
+
+            writer.Indent--;
+            writer.WriteLine("}");
+
+            writer.WriteLine($"public static global::System.Collections.Generic.IEnumerable<{type.MappingType}> MapTo(this global::System.Collections.Generic.IEnumerable<{type.DeclaredType}> input)");
+            writer.WriteLine("{");
+            writer.Indent++;
+
+            writer.WriteLine($"return input.Select(x => x.MapTo());");
+
+            writer.Indent--;
+            writer.WriteLine("}");
+        }
+
+        writer.Indent--;
+        writer.WriteLine("}");
+
+        writer.Indent--;
+        writer.WriteLine("}");
     }
 }
