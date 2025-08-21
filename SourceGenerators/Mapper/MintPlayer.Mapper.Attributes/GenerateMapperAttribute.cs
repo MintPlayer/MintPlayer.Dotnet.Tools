1993cd08ab56f5031c6817419fbf0b8efbf93f98
PieterjanDeClippel <pieterjandeclippel@msn.com>
2025-08-21 23:10:24 +0200

    Basic setup

--- SourceGenerators/Mapper/MintPlayer.Mapper.Attributes/GenerateMapperAttribute.cs
+++ SourceGenerators/Mapper/MintPlayer.Mapper.Attributes/GenerateMapperAttribute.cs
@@ -1,7 +1,7 @@
-﻿namespace MintPlayer.Mapper.Attributes
-{
-    public class GenerateMapperAttribute
-    {
+﻿namespace MintPlayer.Mapper.Attributes;
 
-    }
+[AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct, Inherited = false, AllowMultiple = true)]
+public class GenerateMapperAttribute : Attribute
+{
+
 }
---
+++ SourceGenerators/Mapper/MintPlayer.Mapper/Generators/MapperGenerator.Producer.cs
@@ -0,0 +1,19 @@
+﻿using MintPlayer.Mapper.Models;
+using MintPlayer.SourceGenerators.Tools;
+using System.CodeDom.Compiler;
+
+namespace MintPlayer.Mapper.Generators;
+
+public sealed class MapperProducer : Producer
+{
+    private readonly IEnumerable<TypeToMap> typesToMap;
+    public MapperProducer(IEnumerable<TypeToMap> typesToMap, string rootNamespace) : base(rootNamespace, "Mappers.g.cs")
+    {
+        this.typesToMap = typesToMap;
+    }
+
+    protected override void ProduceSource(IndentedTextWriter writer, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
