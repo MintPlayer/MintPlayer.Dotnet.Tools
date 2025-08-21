69fd4d39d76e2b75b6e8b418c7a7b14985e9a4e2
PieterjanDeClippel <pieterjandeclippel@msn.com>
2025-08-21 23:28:26 +0200

    Correct mapping types

---
+++ SourceGenerators/TestProjects/MapperDebugging/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+using MintPlayer.Mapper.Attributes;
+
+Console.WriteLine("Hello, World!");
+
+[GenerateMapper(typeof(PersonDto))]
+public class Person
+{
+    public string? Name { get; set; }
+    public int? Age { get; set; }
+}
+
+public class PersonDto
+{
+    public string? Name { get; set; }
+    public int? Age { get; set; }
+}
