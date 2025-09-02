c5864d4d8d84e00ffff80a56aabc2a9ef1777f82
PieterjanDeClippel <pieterjandeclippel@msn.com>
2025-09-02 09:29:51 +0200

    Allow multiple

--- SourceGenerators/SourceGenerators/MintPlayer.SourceGenerators.Attributes/RegisterAttribute.cs
+++ SourceGenerators/SourceGenerators/MintPlayer.SourceGenerators.Attributes/RegisterAttribute.cs
@@ -2,8 +2,7 @@
 
 namespace MintPlayer.SourceGenerators.Attributes;
 
-// For now don't allow multiple registrations of the same service
-[AttributeUsage(AttributeTargets.Class, AllowMultiple = false)]
+[AttributeUsage(AttributeTargets.Class, AllowMultiple = true)]
 public class RegisterAttribute : Attribute
 {
     public RegisterAttribute(ServiceLifetime lifetime, string methodNameHint = default, string factory = default) { }
