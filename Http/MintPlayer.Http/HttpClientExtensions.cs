b5f00e3f296e09d858e9407361920881066848e5
PieterjanDeClippel <pieterjandeclippel@msn.com>
2025-09-19 15:15:18 +0200

    FromJsonWithMetaAsync

--- Http/MintPlayer.Http/HttpClientExtensions.cs
+++ Http/MintPlayer.Http/HttpClientExtensions.cs
@@ -9,6 +9,13 @@ public static class HttpClientExtensions
         return value;
     }
 
+    public static async Task<HttpResult<T?>> FromJsonWithMetaAsync<T>(this HttpClient client, HttpRequestMessage message, System.Text.Json.JsonSerializerOptions? options = null, CancellationToken ct = default)
+    {
+        var response = await client.SendAsync(message, ct).ConfigureAwait(false);
+        var value = await response.ReadJsonWithMetaAsync<T>(options, ct).ConfigureAwait(false);
+        return value;
+    }
+
     public static async Task<T?> FromXmlAsync<T>(this HttpClient client, HttpRequestMessage message, CancellationToken ct = default)
     {
         var response = await client.SendAsync(message.WithAccept("text/xml"), ct).ConfigureAwait(false);
@@ -16,10 +23,24 @@ public static class HttpClientExtensions
         return value;
     }
 
+    public static async Task<HttpResult<T?>> FromXmlWithMetaAsync<T>(this HttpClient client, HttpRequestMessage message, CancellationToken ct = default)
+    {
+        var response = await client.SendAsync(message.WithAccept("text/xml"), ct).ConfigureAwait(false);
+        var value = await response.ReadXmlWithMetaAsync<T>(ct).ConfigureAwait(false);
+        return value;
+    }
+
     public static async Task<string> FromTextAsync(this HttpClient client, HttpRequestMessage message, CancellationToken ct = default)
     {
         var response = await client.SendAsync(message.WithAccept("application/json"), ct).ConfigureAwait(false);
         var value = await response.ReadTextAsync(ct).ConfigureAwait(false);
+        return value;
+    }
+
+    public static async Task<HttpResult<string>> FromTextWithMetaAsync(this HttpClient client, HttpRequestMessage message, CancellationToken ct = default)
+    {
+        var response = await client.SendAsync(message.WithAccept("application/json"), ct).ConfigureAwait(false);
+        var value = await response.ReadTextWithMetaAsync(ct).ConfigureAwait(false);
         return value;
     }
 }
