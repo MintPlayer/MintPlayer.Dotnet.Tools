30a2d2ae3f0aeb286dd71bd465acc3d5ddefc337
PieterjanDeClippel <pieterjandeclippel@msn.com>
2025-10-08 14:20:14 +0200

    Temp commit

--- SourceGenerators/TestProjects/CommandLineAppDebugging/Program.cs
+++ SourceGenerators/TestProjects/CommandLineAppDebugging/Program.cs
@@ -1,6 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using MintPlayer.CommandLineApp.Attributes;
 
+Console.WriteLine("test");
+
 [ConsoleApp]
 public class ListAspnetcoreApps
 {
