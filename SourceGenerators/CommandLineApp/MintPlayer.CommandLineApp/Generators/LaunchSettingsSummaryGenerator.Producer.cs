a4779f60a3cd036289e37cc3f42df3d0fd47eea8
PieterjanDeClippel <pieterjandeclippel@msn.com>
2025-10-08 14:01:41 +0200

    Adding console app generator

---
+++ SourceGenerators/CommandLineApp/MintPlayer.CommandLineApp/Generators/LaunchSettingsSummaryGenerator.Producer.cs
@@ -0,0 +1,21 @@
+﻿using MintPlayer.CommandLineApp.Models;
+using MintPlayer.SourceGenerators.Tools;
+using System.CodeDom.Compiler;
+
+namespace MintPlayer.CommandLineApp.Generators;
+
+public class LaunchSettingsSummaryProducer : Producer
+{
+    private readonly IEnumerable<ConsoleApp> consoleApps;
+    public LaunchSettingsSummaryProducer(IEnumerable<Models.ConsoleApp> consoleApps, string rootNamespace) : base(rootNamespace, "LaunchSettingsSummary.g.cs")
+    {
+        this.consoleApps = consoleApps;
+    }
+    
+    protected override void ProduceSource(IndentedTextWriter writer, CancellationToken cancellationToken)
+    {
+        writer.WriteLine("""
+            global::System.Console.WriteLine("Hello, World!");
+            """);
+    }
+}
---
+++ SourceGenerators/CommandLineApp/MintPlayer.CommandLineApp/Generators/LaunchSettingsSummaryGenerator.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using MintPlayer.SourceGenerators.Tools;
+
+namespace MintPlayer.CommandLineApp.Generators
+{
+    [Generator(LanguageNames.CSharp)]
+    public sealed class LaunchSettingsSummaryGenerator : IncrementalGenerator
+    {
+        const string consoleAppAttribute = "MintPlayer.CommandLineApp.Attributes.ConsoleAppAttribute";
+
+        public override void Initialize(IncrementalGeneratorInitializationContext context, IncrementalValueProvider<Settings> settingsProvider)
+        {
+            var consoleAppsProvider = context.SyntaxProvider.ForAttributeWithMetadataName(
+                consoleAppAttribute,
+                (node, ct) => node is ClassDeclarationSyntax { AttributeLists.Count: > 0 },
+                (context, ct) =>
+                {
+                    if (context.TargetNode is ClassDeclarationSyntax classDeclaration &&
+                        context.SemanticModel.GetDeclaredSymbol(classDeclaration, ct) is INamedTypeSymbol classSymbol)
+                    {
+                        return new Models.ConsoleApp
+                        {
+                            ClassName = classSymbol.Name,
+                            Namespace = classSymbol.ContainingNamespace?.ToDisplayString() ?? string.Empty,
+                        };
+                    }
+
+                    return default;
+                })
+                .WithNullableComparer()
+                .Collect();
+
+            var consoleAppsSourceProvider = consoleAppsProvider
+                .Join(settingsProvider)
+                .Select(Producer (prov, ct) => new LaunchSettingsSummaryProducer(prov.Item1, prov.Item2.RootNamespace!));
+
+            context.ProduceCode(consoleAppsSourceProvider);
+        }
+
+        public override void RegisterComparers()
+        {
+        }
+    }
+}
