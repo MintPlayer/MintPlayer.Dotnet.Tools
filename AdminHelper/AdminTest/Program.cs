1352812620657d867a94aa24a4838db2b1d642b6
Pieterjan <pieterjandeclippel@msn.com>
2025-10-09 05:34:48 +0200

    AdminHelper (#115)

---
+++ AdminHelper/AdminTest/Program.cs
@@ -0,0 +1,8 @@
+﻿using MintPlayer.AdminHelper;
+
+AdminHelper.EnsureRunningAsAdmin();
+
+
+Console.WriteLine("Running with administrator privileges!");
+foreach (var arg in Environment.GetCommandLineArgs())
+    Console.WriteLine($"Arg: {arg}");
