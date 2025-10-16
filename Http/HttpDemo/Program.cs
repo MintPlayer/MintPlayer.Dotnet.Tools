e304097d9ef5c3d4ad8162686988dc369d3fe094
PieterjanDeClippel <pieterjandeclippel@msn.com>
2025-10-16 07:17:53 +0200

    Current version

---
+++ Http/HttpDemo/Program.cs
@@ -0,0 +1,12 @@
+﻿using MintPlayer.Http;
+
+using var client = new HttpClient();
+
+var created = await client.FromJsonAsync<WidgetDto>(new HttpRequestMessage(HttpMethod.Post, "https://api.example.com/widgets")
+    .WithAuthorizationBearer("your_jwt_here")
+    .WithHeader("X-TraceId", Guid.NewGuid().ToString())
+    .WithJsonContent(new CreateWidget("Minty", "green")));
+
+
+record CreateWidget(string Name, string Color);
+record WidgetDto(string Id, string Name, string Color);
