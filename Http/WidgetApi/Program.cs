96dbc9c29472a068eec54b438834aba8d340d89b
PieterjanDeClippel <pieterjandeclippel@msn.com>
2025-10-16 09:38:55 +0200

    Working

--- Http/WidgetApi/Program.cs
+++ Http/WidgetApi/Program.cs
@@ -21,26 +21,27 @@ app.Run();
 
 namespace WidgetApi.Contracts
 {
-    [DataContract(Name = nameof(CreateWidget), Namespace = "http://schemas.example.com/widgets")]
+
+    [DataContract(Name = "CreateWidget", Namespace = "http://schemas.example.com/widgets")]
     public class CreateWidget
     {
-        [DataMember] public string Name { get; set; }
-        [DataMember] public string Color { get; set; }
+        [DataMember(Order = 1)] public string Name { get; set; } = default!;
+        [DataMember(Order = 2)] public string Color { get; set; } = default!;
     }
 
-    [DataContract(Name = nameof(WidgetDto), Namespace = "http://schemas.example.com/widgets")]
+    [DataContract(Name = "WidgetDto", Namespace = "http://schemas.example.com/widgets")]
     public class WidgetDto
     {
-        [DataMember] public string Id { get; set; }
-        [DataMember] public string Name { get; set; }
-        [DataMember] public string Color { get; set; }
+        [DataMember(Order = 1)] public string Id { get; set; } = default!;
+        [DataMember(Order = 2)] public string Name { get; set; } = default!;
+        [DataMember(Order = 3)] public string Color { get; set; } = default!;
     }
 
-    [DataContract(Name = nameof(WidgetCreatedResponse), Namespace = "http://schemas.example.com/widgets")]
+    [DataContract(Name = "WidgetCreatedResponse", Namespace = "http://schemas.example.com/widgets")]
     public class WidgetCreatedResponse
     {
-        [DataMember] public WidgetDto Widget { get; set; }
-        [DataMember] public Dictionary<string, string> Headers { get; set; }
+        [DataMember(Order = 1)] public WidgetDto Widget { get; set; } = default!;
+        [DataMember(Order = 2)] public Dictionary<string, string> Headers { get; set; } = default!;
     }
 }
 
@@ -57,7 +58,7 @@ public class WidgetController : ControllerBase
             {
                 Id = Guid.NewGuid().ToString(),
                 Name = input.Name,
-                //Color = input.Color,
+                Color = input.Color,
             },
             Headers = Request.Headers.ToDictionary(h => h.Key, h => string.Join(", ", h.Value.Select(x => x))),
         });
