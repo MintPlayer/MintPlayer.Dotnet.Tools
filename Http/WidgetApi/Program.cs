8c25928fc4f02a83e64654c7fc0a5db54dc8ea2b
PieterjanDeClippel <pieterjandeclippel@msn.com>
2025-10-16 10:07:01 +0200

    Finished api + http

--- Http/WidgetApi/Program.cs
+++ Http/WidgetApi/Program.cs
@@ -21,27 +21,26 @@ app.Run();
 
 namespace WidgetApi.Contracts
 {
-
-    [DataContract(Name = "CreateWidget", Namespace = "http://schemas.example.com/widgets")]
+    [DataContract(Name = nameof(CreateWidget), Namespace = "http://schemas.example.com/widgets")]
     public class CreateWidget
     {
-        [DataMember(Order = 1)] public string Name { get; set; } = default!;
-        [DataMember(Order = 2)] public string Color { get; set; } = default!;
+        [DataMember] public string Name { get; set; } = default!;
+        [DataMember] public string Color { get; set; } = default!;
     }
 
-    [DataContract(Name = "WidgetDto", Namespace = "http://schemas.example.com/widgets")]
+    [DataContract(Name = nameof(WidgetDto), Namespace = "http://schemas.example.com/widgets")]
     public class WidgetDto
     {
-        [DataMember(Order = 1)] public string Id { get; set; } = default!;
-        [DataMember(Order = 2)] public string Name { get; set; } = default!;
-        [DataMember(Order = 3)] public string Color { get; set; } = default!;
+        [DataMember] public string Id { get; set; } = default!;
+        [DataMember] public string Name { get; set; } = default!;
+        [DataMember] public string Color { get; set; } = default!;
     }
 
-    [DataContract(Name = "WidgetCreatedResponse", Namespace = "http://schemas.example.com/widgets")]
+    [DataContract(Name = nameof(WidgetCreatedResponse), Namespace = "http://schemas.example.com/widgets")]
     public class WidgetCreatedResponse
     {
-        [DataMember(Order = 1)] public WidgetDto Widget { get; set; } = default!;
-        [DataMember(Order = 2)] public Dictionary<string, string> Headers { get; set; } = default!;
+        [DataMember] public WidgetDto Widget { get; set; } = default!;
+        [DataMember] public Dictionary<string, string> Headers { get; set; } = default!;
     }
 }
 
