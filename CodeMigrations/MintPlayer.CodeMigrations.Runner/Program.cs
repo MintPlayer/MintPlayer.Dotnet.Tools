abd1f65de7a27439d6170e59dbcaff3d577c0241
PieterjanDeClippel <pieterjandeclippel@msn.com>
2025-11-15 14:16:07 +0100

    temp commit

--- CodeMigrations/MintPlayer.CodeMigrations.Runner/Program.cs
+++ CodeMigrations/MintPlayer.CodeMigrations.Runner/Program.cs
@@ -2,5 +2,24 @@
 // or
 // dnx MintPlayer.CodeMigrations
 
-Console.WriteLine("Current directory");
-Console.WriteLine(Directory.GetCurrentDirectory());
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using MintPlayer.CodeMigrations.Tools;
+
+var builder = Host.CreateApplicationBuilder(args);
+
+var currentDirectory = Directory.GetCurrentDirectory();
+var migrationsPath = Path.Combine(currentDirectory, "migrations.json");
+builder.Configuration.AddJsonFile(migrationsPath, optional: true);
+
+builder.Logging.ClearProviders();
+builder.Logging.AddConsole();
+
+builder.Services.AddSingleton(provider =>
+{
+    var migrationConfig = new MigrationConfig { PackageName =  };
+    provider.GetRequiredService<IConfiguration>().Bind(migrationConfig);
+    return migrationConfig;
+});
